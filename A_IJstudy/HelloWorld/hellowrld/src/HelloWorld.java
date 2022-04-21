aa6d5e435fd6b9186cb1a9426c148500141abdd7
ChandlerPJX <jinxinpang@icloud.com>
2022-04-21 10:30:47 +0800

    修改

--- A_IJstudy/HelloWorld/hellowrld/src/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld
-{
-    public static void main(String[] args)
-    {
-        // 输出helloWorld！
-        System.out.println("Hello World");
-    }
-}
--- A_IJstudy/untitled/基础语法/src/test.java
+++
@@ -1,8 +0,0 @@
-package src;
-
-public class test {
-    public static void main(String[] args) {
-        System.out.println("Hello\tWorld \n 运行无碍！");
-    }
-
-}
